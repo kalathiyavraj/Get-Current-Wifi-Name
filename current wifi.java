16e0b4ac1d7485652e27a3bcde1350ef9e708cfb
kalathiya vraj <84270801+kalathiyavraj@users.noreply.github.com>
2024-04-08 10:55:37 +0530

    current wifi.java

---
+++ current wifi.java
@@ -0,0 +1,49 @@
+//add permission
+    <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+
+//call this
+
+getCurrentSsid(HomeActivity.this);
+
+//method
+
+    public String getCurrentSsid(Context context) {
+
+        String ssid = null;
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (networkInfo.isConnected()) {
+            final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            if (connectionInfo != null && !(connectionInfo.getSSID().equals(""))) {
+                //if (connectionInfo != null && !StringUtil.isBlank(connectionInfo.getSSID())) {
+                ssid = connectionInfo.getSSID();
+            }
+            // Get WiFi status MARAKANA
+            WifiInfo info = wifiManager.getConnectionInfo();
+            String textStatus = "";
+            textStatus += "\n\nWiFi Status: " + info.toString();
+            String BSSID = info.getBSSID();
+            String MAC = info.getMacAddress();
+
+            List<ScanResult> results = wifiManager.getScanResults();
+            ScanResult bestSignal = null;
+            int count = 1;
+            String etWifiList = "";
+            for (ScanResult result : results) {
+                etWifiList += count++ + ". " + result.SSID + " : " + result.level + "\n" +
+                        result.BSSID + "\n" + result.capabilities +"\n" +
+                        "\n=======================\n";
+            }
+            Log.v("TAGGGGGGGGGGG", "from SO: \n"+etWifiList);
+
+            // List stored networks
+            List<WifiConfiguration> configs = wifiManager.getConfiguredNetworks();
+            for (WifiConfiguration config : configs) {
+                textStatus+= "\n\n" + config.toString();
+            }
+            Log.v("TAGGGGGGGGGGG","from marakana: \n"+textStatus);
+        }
+        return ssid;
+    }
